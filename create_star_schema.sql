e4eda0d0e67aa26f9bbc7f8545552e6375ec1ec5
kajamalie <69890213+kajamalie@users.noreply.github.com>
2020-09-04 08:32:35 +0200

    nyeste_kaja

--- create_star_schema.sql
+++ create_star_schema.sql
@@ -83,6 +83,167 @@ insert into csv_data.time_hour
 
 
 
+
+
+--Nyde tidstabeller: 
+CREATE table star_times (
+    id int4 NOT NULL,
+    time time,
+    hour int2,
+    military_hour int2,
+    minute int4,
+    second int4,
+    minute_of_day int4,
+    second_of_day int4,
+    quarter_hour varchar,
+    am_pm varchar,
+    day_night varchar,
+    day_night_abbrev varchar,
+    time_period varchar,
+    time_period_abbrev varchar
+)
+WITH (OIDS=FALSE);
+TRUNCATE TABLE star.times;
+-- Unknown member
+INSERT INTO star.times VALUES (
+    -1, --id
+    '0:0:0', -- time
+    0, -- hour
+    0, -- military_hour
+    0, -- minute
+    0, -- second
+    0, -- minute_of_day
+    0, -- second_of_day
+    'Unknown', -- quarter_hour
+    'Unknown', -- am_pm
+    'Unknown', -- day_night
+    'Unk', -- day_night_abbrev
+    'Unknown', -- time_period
+    'Unk' -- time_period_abbrev
+);
+INSERT INTO star_times
+SELECT
+  to_char(datum, 'HH24MISS')::integer AS id,
+  datum::time AS time,
+  to_char(datum, 'HH12')::integer AS hour,
+  to_char(datum, 'HH24')::integer AS military_hour,
+  extract(minute FROM datum)::integer AS minute,
+  extract(second FROM datum) AS second,
+  to_char(datum, 'SSSS')::integer / 60 AS minute_of_day,
+  to_char(datum, 'SSSS')::integer AS second_of_day,
+  to_char(datum - (extract(minute FROM datum)::integer % 15 || 'minutes')::interval, 'hh24:mi') ||
+  ' – ' ||
+  to_char(datum - (extract(minute FROM datum)::integer % 15 || 'minutes')::interval + '14 minutes'::interval, 'hh24:mi')
+    AS quarter_hour,
+  to_char(datum, 'AM') AS am_pm,
+  CASE WHEN to_char(datum, 'hh24:mi') BETWEEN '08:00' AND '19:59' THEN 'Day (8AM-8PM)' ELSE 'Night (8PM-8AM)' END
+  AS day_night,
+  CASE WHEN to_char(datum, 'hh24:mi') BETWEEN '08:00' AND '19:59' THEN 'Day' ELSE 'Night' END
+  AS day_night_abbrev,
+  CASE
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '00:00' AND '03:59' THEN 'Late Night (Midnight-4AM)'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '04:00' AND '07:59' THEN 'Early Morning (4AM-8AM)'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '08:00' AND '11:59' THEN 'Morning (8AM-Noon)'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '12:00' AND '15:59' THEN 'Afternoon (Noon-4PM)'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '16:00' AND '19:59' THEN 'Evening (4PM-8PM)'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '20:00' AND '23:59' THEN 'Night (8PM-Midnight)'
+  END AS time_period,
+  CASE
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '00:00' AND '03:59' THEN 'Late Night'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '04:00' AND '07:59' THEN 'Early Morning'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '08:00' AND '11:59' THEN 'Morning'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '12:00' AND '15:59' THEN 'Afternoon'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '16:00' AND '19:59' THEN 'Evening'
+  WHEN to_char(datum, 'hh24:mi') BETWEEN '20:00' AND '23:59' THEN 'Night'
+  END AS time_period_abbrev
+FROM generate_series('2000-01-01 00:00:00'::timestamp, '2000-01-01 23:59:59'::timestamp, '1 hour') datum;
+
+ALTER TABLE  csv_data.star_times 
+   ADD column s_times_id serial primary key;
+-- Dimension: Date
+-- PK: k_date (YYYYMMDD)
+-- Variable: all different time formats :-)
+DROP TABLE if exists star.date_s;
+CREATE TABLE star_date
+(
+  k_date              INT NOT NULL,
+  date_actual              DATE NOT NULL,
+  epoch                    BIGINT NOT NULL,
+  day_suffix               VARCHAR(4) NOT NULL,
+  day_name                 VARCHAR(9) NOT NULL,
+  day_of_week              INT NOT NULL,
+  day_of_month             INT NOT NULL,
+  day_of_quarter           INT NOT NULL,
+  day_of_year              INT NOT NULL,
+  week_of_month            INT NOT NULL,
+  week_of_year             INT NOT NULL,
+  week_of_year_iso         CHAR(10) NOT NULL,
+  month_actual             INT NOT NULL,
+  month_name               VARCHAR(9) NOT NULL,
+  month_name_abbreviated   CHAR(3) NOT NULL,
+  quarter_actual           INT NOT NULL,
+  quarter_name             VARCHAR(9) NOT NULL,
+  year_actual              INT NOT NULL,
+  first_day_of_week        DATE NOT NULL,
+  last_day_of_week         DATE NOT NULL,
+  first_day_of_month       DATE NOT NULL,
+  last_day_of_month        DATE NOT NULL,
+  first_day_of_quarter     DATE NOT NULL,
+  last_day_of_quarter      DATE NOT NULL,
+  first_day_of_year        DATE NOT NULL,
+  last_day_of_year         DATE NOT NULL,
+  mmyyyy                   CHAR(6) NOT NULL,
+  mmddyyyy                 CHAR(10) NOT NULL,
+  weekend_indr             BOOLEAN NOT NULL
+);
+ALTER TABLE star_date ADD CONSTRAINT date_s_k_date_pk PRIMARY KEY (k_date);
+CREATE INDEX date_s_date_actual_idx
+  ON star.date_s(date_actual);
+COMMIT;
+INSERT INTO star_date
+SELECT TO_CHAR(datum, 'yyyymmdd')::INT AS k_date,
+       datum AS date_actual,
+       EXTRACT(EPOCH FROM datum) AS epoch,
+       TO_CHAR(datum, 'fmDDth') AS day_suffix,
+       TO_CHAR(datum, 'Day') AS day_name,
+       EXTRACT(ISODOW FROM datum) AS day_of_week,
+       EXTRACT(DAY FROM datum) AS day_of_month,
+       datum - DATE_TRUNC('quarter', datum)::DATE + 1 AS day_of_quarter,
+       EXTRACT(DOY FROM datum) AS day_of_year,
+       TO_CHAR(datum, 'W')::INT AS week_of_month,
+       EXTRACT(WEEK FROM datum) AS week_of_year,
+       EXTRACT(ISOYEAR FROM datum) || TO_CHAR(datum, '"-W"IW-') || EXTRACT(ISODOW FROM datum) AS week_of_year_iso,
+       EXTRACT(MONTH FROM datum) AS month_actual,
+       TO_CHAR(datum, 'Month') AS month_name,
+       TO_CHAR(datum, 'Mon') AS month_name_abbreviated,
+       EXTRACT(QUARTER FROM datum) AS quarter_actual,
+       CASE
+           WHEN EXTRACT(QUARTER FROM datum) = 1 THEN 'First'
+           WHEN EXTRACT(QUARTER FROM datum) = 2 THEN 'Second'
+           WHEN EXTRACT(QUARTER FROM datum) = 3 THEN 'Third'
+           WHEN EXTRACT(QUARTER FROM datum) = 4 THEN 'Fourth'
+           END AS quarter_name,
+       EXTRACT(ISOYEAR FROM datum) AS year_actual,
+       datum + (1 - EXTRACT(ISODOW FROM datum))::INT AS first_day_of_week,
+       datum + (7 - EXTRACT(ISODOW FROM datum))::INT AS last_day_of_week,
+       datum + (1 - EXTRACT(DAY FROM datum))::INT AS first_day_of_month,
+       (DATE_TRUNC('MONTH', datum) + INTERVAL '1 MONTH - 1 day')::DATE AS last_day_of_month,
+       DATE_TRUNC('quarter', datum)::DATE AS first_day_of_quarter,
+       (DATE_TRUNC('quarter', datum) + INTERVAL '3 MONTH - 1 day')::DATE AS last_day_of_quarter,
+       TO_DATE(EXTRACT(YEAR FROM datum) || '-01-01', 'YYYY-MM-DD') AS first_day_of_year,
+       TO_DATE(EXTRACT(YEAR FROM datum) || '-12-31', 'YYYY-MM-DD') AS last_day_of_year,
+       TO_CHAR(datum, 'mmyyyy') AS mmyyyy,
+       TO_CHAR(datum, 'mmddyyyy') AS mmddyyyy,
+       CASE
+           WHEN EXTRACT(ISODOW FROM datum) IN (6, 7) THEN TRUE
+           ELSE FALSE
+           END AS weekend_indr
+FROM (SELECT '2000-01-01'::DATE + SEQUENCE.DAY AS datum
+      FROM GENERATE_SERIES(0, 29219) AS SEQUENCE (DAY)
+      GROUP BY SEQUENCE.DAY) DQ
+ORDER BY 1;
+
+
 -- create place dim: bus_stop
        
 SELECT stop_id, stop_name
@@ -115,35 +276,19 @@ create table csv_data.fact (
   	   fact_id serial primary key,
        s_stop_id int references  csv_data.bus_stop(s_stop_id),
        s_weather_id int references csv_data.weather_station(s_weather_id),
-       date_hour varchar references csv_data.time_hour(date_hour), 
-       date_actual date references csv_data.date_time(date_actual),
+       time_hour int references csv_data.star_times(s_times_id), 
+       date_actual int references csv_data.star_date(k_date),
        temperature varchar, 
        precipitation varchar
       ); 
-     
-     
 
+
+   
 
     --start på join for å populere fact_table:
-    
-     
-     
-     
+
+  --med startimes         
 with sourses as (select so.weather_stat_id, so.timereference, so.temperature, so.percipitation, ws.s_weather_id, csw.ruter_stop, SO.timereference 
-                 from public.stage_observations so 
-                 join csv_data.weather_station ws on so.weather_stat_id = ws.source_id
-                 join public.closest_weather_station csw on ws.source_id = csw.weather_stat_id)    
-insert into csv_data.fact (s_stop_id, s_weather_id, date_hour, date_actual, temperature, precipitation)
-            select bs.s_stop_id,  ws.s_weather_id, so.temperature, so.percipitation 
-            from sourses s
-            join csv_data.bus_stop bs on s.ruter_stop = bs.stop_name 
-            join csv_data.weather_station ws using(s_weather_id)
-            join csv_data.time_hour th on th.date_hour = s.timereference
-            join csv_data.date_time dt using(date_actual); 
-           
-       --uten tider(denne er kjørt nå):   
-           
-with sourses as (select so.weather_stat_id, so.temperature, so.percipitation, ws.s_weather_id, csw.ruter_stop 
                  from public.stage_observations so 
                  join csv_data.weather_station ws on so.weather_stat_id = ws.source_id
                  join public.closest_weather_station csw on ws.source_id = csw.weather_stat_id)    
@@ -152,9 +297,38 @@ insert into csv_data.fact (s_stop_id, s_weather_id, temperature, precipitation)
             from sourses s
             join csv_data.bus_stop bs on s.ruter_stop = bs.stop_name 
             join csv_data.weather_station ws using(s_weather_id)
-            --join csv_data.time_hour th on th.date_hour = s.timereference
-            --join csv_data.date_time dt using(date_actual); 
+            --join csv_data.star_times st st on references (s_times_id)
+            --join csv_data.star_date sd using(k_date); 
            
-  
-
-
+            
+            --til når vi skal få inn tider:
+             st."hour", sd.date_actual,
+           time_hour, date_actual,
+           
+           
+           select * from csv_data.fact f ;
+          select * from public.stage_observations so ;
+         select * from csv_data.weather_station ws ;
+        
+        select * from csv_data.star_date sd ;
+       select * from csv_data.star_times st ;
+      
+      
+      
+      
+   with sources as (select so.weather_stat_id, so.timereference, so.temperature, so.percipitation, ws.s_weather_id, csw.ruter_stop, SO.timereference 
+                 from public.stage_observations so 
+                 join csv_data.weather_station ws on so.weather_stat_id = ws.source_id
+                 join public.closest_weather_station csw on ws.source_id = csw.weather_stat_id)
+        insert into csv_data.fact (s_stop_id, s_weather_id, time_hour, date_actual, temperature, precipitation)
+               from sources s
+               join 
+                 ;
+                
+                
+                
+   select ws.ruter_stop, ws.weather_stat_id, so."date", so.observation_id, so.percipitation, so.temperature, so."time", so.weather_stat_id 
+              from public.closest_weather_station ws
+              join public.stage_observations2 so using(weather_stat_id); 
+          
+           
